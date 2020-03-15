7589b8154434f4fb5c6822aad1cdb7cb88443dae
Diogo Francisco Goncalves de Aguiar Camacho <diogo.camacho@ibm.com>
2020-03-15 18:37:16 -0300

    Answer

--- Exercise_three.js
+++ Exercise_three.js
@@ -16,3 +16,28 @@ Passing this string to console.log should show something like this:
 When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 
 */
+
+let size = 8;
+let grid = "";
+for(let iterator = 0; iterator < size; iterator++)
+{
+    if(iterator % 2 == 0)
+    {
+        for(let itertatorGrid = 0; itertatorGrid < size; itertatorGrid++)
+        {
+            itertatorGrid % 2 == 0?grid += " ": grid+="#";
+        }
+        grid += '\n'
+    }
+
+    if(iterator % 2 != 0)
+    {
+        for(let itertatorGrid = 0; itertatorGrid < size; itertatorGrid++)
+        {
+            itertatorGrid % 2 != 0?grid += " ": grid+="#";
+        }
+        grid += '\n'
+    }
+}
+
+console.log(grid);
